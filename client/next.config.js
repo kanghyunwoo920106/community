1c2294c3800a33134781e55906f65bfccde07d05
hyunwoo <65879478+kanghyunwoo0106@users.noreply.github.com>
2023-04-26 01:13:54 +0900

    next config 수정

--- client/next.config.js
+++ client/next.config.js
@@ -4,9 +4,6 @@ const nextConfig = {
   images: {
     domains: ['www.gravatar.com', 'localhost', 'ec2-15-164-163-51.ap-northeast-2.compute.amazonaws.com'],
   },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
 };
 
 module.exports = nextConfig;
