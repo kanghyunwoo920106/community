5fcd569d2f17712d0447e8c196c103eb688283d5
hyunwoo <65879478+kanghyunwoo0106@users.noreply.github.com>
2023-04-25 18:18:39 +0900

    이미지 사용위해 실서버 도메인 추가

--- client/next.config.js
+++ client/next.config.js
@@ -2,7 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['www.gravatar.com', 'localhost'],
+    domains: [
+      'www.gravatar.com',
+      'localhost',
+      'http://ec2-15-164-163-51.ap-northeast-2.compute.amazonaws.com',
+    ],
   },
   basePath: '/client',
 };
