fc0c2136746b97f23cc62d61c6bc50dbd8d1345b
hyunwoo <65879478+kanghyunwoo0106@users.noreply.github.com>
2023-04-27 00:02:43 +0900

    로그아웃 secure 설정 변경

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -109,7 +109,7 @@ const logout = async (req: Request, res: Response) => {
     "Set-Cookie",
     cookie.serialize("token", "", {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+      // secure: process.env.NODE_ENV === "production",
       sameSite: "strict",
       expires: new Date(0),
       path: "/",
